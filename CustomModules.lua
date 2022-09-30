636b1cae175bcbff5e4460921c4fc00d29201726
NemoExeYT <70239152+NEMOEXEYT@users.noreply.github.com>
2022-10-01 01:36:33 +0300

    Create CustomModules.lua

---
+++ CustomModules.lua
@@ -0,0 +1,25 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+repeat
+    task.wait()
+until shared.GuiLibrary
+local GuiLibrary = shared.GuiLibrary
+local ScriptSettings = {}
+local UIS = game:GetService("UserInputService")
+local COB = function(tab, argstable)
+
+end
+
+infjump = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].CreateOptionsButton({
+    ["Name"] = "infinite Jump",
+    ["Function"] = function(callback)
+        local InfiniteJumpEnabled = true
+        game:GetService("UserInputService").JumpRequest:connect(function()
+            if InfiniteJumpEnabled then
+                game:GetService "Players".LocalPlayer.Character:FindFirstChildOfClass 'Humanoid':ChangeState("Jumping")
+            end
+        end)
+    end,
+    ["HoverText"] = "Makes You Infinite Jump."
+})
