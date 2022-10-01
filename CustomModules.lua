d91634052bce3b38603eae61adc5338040822e84
NemoExeYT <70239152+NEMOEXEYT@users.noreply.github.com>
2022-10-02 01:42:14 +0300

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -23,3 +23,41 @@ InfiniteJump = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].Creat
     end,
     ["HoverText"] = "Makes you infinite jump"
 })
+
+
+
+
+
+local actog = false
+	local actogtick = tick()
+	ac = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
+		["Name"] = "AcDisabler2",
+		["Function"] = function(callback)
+			if callback then
+	         local vec3 = Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.X + 39, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Y + 12, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z)
+local bp = Instance.new('BodyPosition')
+bp.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+bp.Position = vec3
+wait(1)
+bp:remove()
+local bv = Instance.new("BodyVelocity")
+bv.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+bv.Velocity = Vector3.new(0, -25, 0)
+wait(.2)
+  
+
+local bpfwd = Instance.new("BodyPosition")
+bpfwd.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+bpfwd.Position = Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X + 74, game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y + 25, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z)
+bpfwd.P = 10000
+bpfwd.D = 0
+
+wait(.1)
+bv:remove()
+wait(.4)
+bpfwd:remove()
+
+			end
+		end,
+		["HoverText"] = "Disables 50% of the Anti Cheat."
+	})
