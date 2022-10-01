710e80e3adbe48ded25567dedd0113ddecb668c7
NemoExeYT <70239152+NEMOEXEYT@users.noreply.github.com>
2022-10-02 01:06:09 +0300

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -23,3 +23,25 @@ InfiniteJump = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].Creat
     end,
     ["HoverText"] = "Makes You Infinite Jump."
 })
+
+
+
+
+
+local acdis2 = COB("Blatant", {
+	["Name"] = "50%AcDisabler",
+	["HoverText"] = "Disables 50% of the ac",
+	["Function"] = function(callback)
+		if callback then
+             local bpfwd = Instance.new("BodyPosition")
+            bpfwd.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+            bpfwd.Position = Vector3.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position.X + 74, game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y + 25, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z)
+            bpfwd.P = 10000
+            bpfwd.D = 0
+            wait(.1)
+             bv:remove()
+             wait(.4)
+           bpfwd:remove()
+         end
+	end
+})
